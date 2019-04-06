94e2f873c88a795303737cb23e7116e4b25ac465
Kenneth Evans <ken@kenevans.net>
2019-04-05 20:09:21 -0400

    Refactored to AndroidX. versionCode=5. versionName=2.0.0.

--- app/src/main/java/net/kenevans/android/appinfo/InfoActivity.java
+++ app/src/main/java/net/kenevans/android/appinfo/InfoActivity.java
@@ -21,9 +21,8 @@
 
 package net.kenevans.android.appinfo;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.webkit.WebView;
 
